667a9bcdc16181ca2a54bd7df55aeb64e6de7803
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-04 18:00:57 +0900

    DB

--- 勤務表管理作成/SQL/M_USER.sql
+++ 勤務表管理作成/SQL/M_USER.sql
@@ -4,12 +4,14 @@
 
 CREATE TABLE public."M_USER"
 (
-  "ID" character varying(6), -- 社員ID
+  "USER_ID" character varying(6) NOT NULL, -- ユーザID
+  "USER_NAME" character varying(6), -- ユーザ名
   "INSERT_DATE" time with time zone, -- 追加日時
   "INSERT_USER_ID" character varying(6), -- 追加ユーザID
   "UPDATE_DATE" time with time zone, -- 更新日時
   "UPDATE_USER_ID" character varying(6), -- 更新ユーザID
-  "ENABLE_FLG" smallint -- 有効フラグ｜0:無効,1:有効
+  "ENABLE_FLG" smallint, -- 有効フラグ｜0:無効,1:有効
+  CONSTRAINT "M_USER_pkey" PRIMARY KEY ("USER_ID")
 )
 WITH (
   OIDS=FALSE
@@ -17,8 +19,9 @@ WITH (
 ALTER TABLE public."M_USER"
   OWNER TO postgres;
 COMMENT ON TABLE public."M_USER"
-  IS '社員テーブル';
-COMMENT ON COLUMN public."M_USER"."ID" IS '社員ID';
+  IS 'ユーザテーブル';
+COMMENT ON COLUMN public."M_USER"."USER_ID" IS 'ユーザID';
+COMMENT ON COLUMN public."M_USER"."USER_NAME" IS 'ユーザ名';
 COMMENT ON COLUMN public."M_USER"."INSERT_DATE" IS '追加日時';
 COMMENT ON COLUMN public."M_USER"."INSERT_USER_ID" IS '追加ユーザID';
 COMMENT ON COLUMN public."M_USER"."UPDATE_DATE" IS '更新日時';
