fbe7f31adbaae220d68518ecb79016217b652774
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-04 17:36:22 +0900

    db

--- 勤務表管理作成/SQL/M_USER.sql
+++ 勤務表管理作成/SQL/M_USER.sql
@@ -4,7 +4,8 @@
 
 CREATE TABLE public."M_USER"
 (
-  "ID" text -- 社員ID
+  "ID" character varying(6), -- 社員ID
+  "INSERT_DATE" time with time zone -- 追加日時
 )
 WITH (
   OIDS=FALSE
@@ -14,4 +15,5 @@ ALTER TABLE public."M_USER"
 COMMENT ON TABLE public."M_USER"
   IS '社員テーブル';
 COMMENT ON COLUMN public."M_USER"."ID" IS '社員ID';
+COMMENT ON COLUMN public."M_USER"."INSERT_DATE" IS '追加日時';
 
