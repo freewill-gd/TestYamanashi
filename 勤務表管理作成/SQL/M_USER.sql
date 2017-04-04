17e175e7fdfc89425dfff8489292d5a9e56e9c3c
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-04 17:53:33 +0900

    DB

--- 勤務表管理作成/SQL/M_USER.sql
+++ 勤務表管理作成/SQL/M_USER.sql
@@ -5,7 +5,11 @@
 CREATE TABLE public."M_USER"
 (
   "ID" character varying(6), -- 社員ID
-  "INSERT_DATE" time with time zone -- 追加日時
+  "INSERT_DATE" time with time zone, -- 追加日時
+  "INSERT_USER_ID" character varying(6), -- 追加ユーザID
+  "UPDATE_DATE" time with time zone, -- 更新日時
+  "UPDATE_USER_ID" character varying(6), -- 更新ユーザID
+  "ENABLE_FLG" smallint -- 有効フラグ｜0:無効,1:有効
 )
 WITH (
   OIDS=FALSE
@@ -16,4 +20,8 @@ COMMENT ON TABLE public."M_USER"
   IS '社員テーブル';
 COMMENT ON COLUMN public."M_USER"."ID" IS '社員ID';
 COMMENT ON COLUMN public."M_USER"."INSERT_DATE" IS '追加日時';
+COMMENT ON COLUMN public."M_USER"."INSERT_USER_ID" IS '追加ユーザID';
+COMMENT ON COLUMN public."M_USER"."UPDATE_DATE" IS '更新日時';
+COMMENT ON COLUMN public."M_USER"."UPDATE_USER_ID" IS '更新ユーザID';
+COMMENT ON COLUMN public."M_USER"."ENABLE_FLG" IS '有効フラグ｜0:無効,1:有効';
 
