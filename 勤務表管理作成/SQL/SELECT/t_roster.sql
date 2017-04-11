14d5dd160ff8de2e4000781862ac5b3a7ddf203c
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-11 15:02:33 +0900

    test

---
+++ 勤務表管理作成/SQL/SELECT/t_roster.sql
@@ -0,0 +1,22 @@
+﻿SELECT user_id
+ , work_date
+ , holiday_flg
+ , work_kind
+ , start_time
+ , end_time
+ , break_time
+ , over_time
+ , late_night_over_time
+ , holiday_over_time
+ , holiday_late_night_over_time
+ , remarks
+ , insert_date
+ , insert_user_id
+ , update_date
+ , update_user_id
+ , enable_flg
+FROM 
+  t_roster
+WHERE
+ work_date >= TO_DATE('2017/04', 'YYYY/MM')
+ AND work_date < TO_DATE('2017/05', 'YYYY/MM')
---
+++ 勤務表管理作成/workspace/GodWork/WEB-INF/src/freewill/database/TestDB.java
@@ -0,0 +1,17 @@
+package freewill.database;
+
+import freewill.database.DataAccess;
+
+public class TestDB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		DataAccess da = new DataAccess();
+		System.out.println(da.getText());
+
+	}
+
+}
