8590a5be485dacfdc35624f3cfc58363a79e98ea
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-12 16:24:13 +0900

    K

--- 勤務表管理作成/SQL/TESTDATA/roster.sql
+++ 勤務表管理作成/SQL/TESTDATA/roster.sql
@@ -1,11 +1,47 @@
+DELETE FROM t_roster;
+SELECT prc_create_month_roster('fw001', 2017, 1);
+SELECT prc_create_month_roster('fw001', 2017, 2);
+SELECT prc_create_month_roster('fw001', 2017, 3);
+SELECT prc_create_month_roster('fw001', 2017, 4);
+SELECT prc_create_month_roster('fw001', 2017, 5);
+SELECT prc_create_month_roster('fw001', 2017, 6);
+SELECT prc_create_month_roster('fw001', 2017, 7);
+SELECT prc_create_month_roster('fw001', 2017, 8);
+SELECT prc_create_month_roster('fw001', 2017, 9);
+SELECT prc_create_month_roster('fw001', 2017, 10);
+SELECT prc_create_month_roster('fw001', 2017, 11);
+SELECT prc_create_month_roster('fw001', 2017, 12);
+
+SELECT prc_create_month_roster('fw001', 2018, 1);
+SELECT prc_create_month_roster('fw001', 2018, 2);
+SELECT prc_create_month_roster('fw001', 2018, 3);
+SELECT prc_create_month_roster('fw001', 2018, 4);
+SELECT prc_create_month_roster('fw001', 2018, 5);
+SELECT prc_create_month_roster('fw001', 2018, 6);
+SELECT prc_create_month_roster('fw001', 2018, 7);
+SELECT prc_create_month_roster('fw001', 2018, 8);
+SELECT prc_create_month_roster('fw001', 2018, 9);
+SELECT prc_create_month_roster('fw001', 2018, 10);
+SELECT prc_create_month_roster('fw001', 2018, 11);
+SELECT prc_create_month_roster('fw001', 2018, 12);
+
 UPDATE t_roster
 SET start_time = work_date + '9 hour'::interval
    , end_time = work_date + '23 hour'::interval
    , break_time = 60
    , over_time = 4 * 60
    , late_night_over_time = 60
-   , holiday_over_time = 0
-   , holiday_late_night_over_time = 0
-   , remarks = '備考のこの野郎!'
+   , remarks = '備考だこの野郎!'
+WHERE fn_weekday(work_date) NOT IN('土', '日')
 ;
-commit;
+
+UPDATE t_roster
+SET start_time = work_date + '9 hour'::interval
+   , end_time = work_date + '23 hour'::interval
+   , break_time = 60
+   , holiday_over_time = 60
+   , holiday_late_night_over_time = 4 * 60
+   , remarks = '休みだ！文句あるか、社畜だこの野郎!'
+WHERE fn_weekday(work_date) IN ('土', '日')
+;
+
