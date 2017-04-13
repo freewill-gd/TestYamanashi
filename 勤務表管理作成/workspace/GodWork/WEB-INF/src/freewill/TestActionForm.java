2a6856497962a1f532640b48dad528d0bf87a450
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-13 10:48:58 +0900

    l

--- 勤務表管理作成/workspace/GodWork/WEB-INF/src/freewill/TestActionForm.java
+++ 勤務表管理作成/workspace/GodWork/WEB-INF/src/freewill/TestActionForm.java
@@ -1,17 +1,29 @@
 package freewill;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.struts.action.ActionForm;
 
 public final class TestActionForm extends ActionForm {
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private String text;
+  public List<Map<String, Object>> data;
 
-	public String getText() {
-		return text;
-	}
+  public List<Map<String, Object>> getData() {
+	return data;
+}
 
-	public void setText(String text) {
-		this.text = text;
-	}
+public void setData(List<Map<String, Object>> data) {
+	this.data = data;
+}
+
+private String text;
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
 }
