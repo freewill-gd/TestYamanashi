46b4b3a9a5c332d36ff31a1801d1c6ddbb252226
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-21 21:49:10 +0900

    パスワード

--- GodWork/SQL/TABLE/m_user.sql
+++ GodWork/SQL/TABLE/m_user.sql
@@ -6,6 +6,7 @@ CREATE TABLE public.m_user
 (
   user_id character varying(6) NOT NULL, -- ユーザID
   user_name character varying(6), -- ユーザ名
+  password character varying(20), -- パスワード
   insert_date time with time zone, -- 追加日時
   insert_user_id character varying(6), -- 追加ユーザID
   update_date time with time zone, -- 更新日時
@@ -22,9 +23,11 @@ COMMENT ON TABLE public.m_user
   IS 'ユーザテーブル';
 COMMENT ON COLUMN public.m_user.user_id IS 'ユーザID';
 COMMENT ON COLUMN public.m_user.user_name IS 'ユーザ名';
+COMMENT ON COLUMN public.m_user.password IS 'パスワード';
 COMMENT ON COLUMN public.m_user.insert_date IS '追加日時';
 COMMENT ON COLUMN public.m_user.insert_user_id IS '追加ユーザID';
 COMMENT ON COLUMN public.m_user.update_date IS '更新日時';
 COMMENT ON COLUMN public.m_user.update_user_id IS '更新ユーザID';
 COMMENT ON COLUMN public.m_user.enable_flg IS '有効フラグ｜0:無効,1:有効';
+
 
