aee9daaa6e4c4cda8cf43764a6323d746ecd2e38
user <user@user-Aspire-ES1-111M>
2017-04-21 13:01:17 +0900

    エラーチェック

--- GodWork/workspace/GodWork/js/roster.js
+++ GodWork/workspace/GodWork/js/roster.js
@@ -1,13 +1,45 @@
+$(document).ready(init);
+
+// 月移動
 function move() {
 	goAction("move");
 }
 
+// 勤務表更新
 function update() {
 	goAction("update");
 }
 
+// 押されたボタンを設定し送信
 function goAction(mode) {
 	var obj  = document.getElementById("actionMode");
 	obj.value = mode;
 	document.forms[0].submit();
+}
+
+
+// ページ初期化
+function init() {
+	$('.time_text').blur(checkTime);
+ }
+ 
+
+//　時間フォーマットチェック後テキストボックスの背景を変更
+function checkTime(){
+	textObject = $(this);
+	var text = textObject.val();
+	if (checkTimeString(text)) {
+		textObject.removeClass('error_text');
+	}
+	else {
+		textObject.addClass('error_text');
+	}
+
+}
+
+// 時間のフォーマットチェック
+function checkTimeString(timestr) {
+	if(timestr.length == 0) return true;
+	var ptr = /^[0-9][0-9]:[0-5][0-9]$/;
+	return ptr.test(timestr);
 }
