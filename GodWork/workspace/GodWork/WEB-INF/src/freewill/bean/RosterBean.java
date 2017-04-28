b87e7fd7ba88cf139c448165d94e44610103fac8
Tomoyuki Yamanashi <s-yamachan@freewill-gd.co.jp>
2017-04-28 17:48:01 +0900

    コメント

--- GodWork/workspace/GodWork/WEB-INF/src/freewill/bean/RosterBean.java
+++ GodWork/workspace/GodWork/WEB-INF/src/freewill/bean/RosterBean.java
@@ -3,9 +3,7 @@ package freewill.bean;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedHashMap;
-
 import org.apache.struts.action.ActionForm;
-
 import freewill.database.dto.RosterDto;
 
 /**
@@ -74,56 +72,59 @@ public final class RosterBean extends ActionForm {
 				data[i] = new RosterDto();
 		}
 	}
-
-	public String getActionMode() {
-		return actionMode;
-	}
-
-	public void setActionMode(String actionMode) {
-		this.actionMode = actionMode;
-	}
-
-
-	public void setSelectYear(LinkedHashMap<String, String> selectYear) {
-		this.selectYear = selectYear;
-	}
-
-	public LinkedHashMap<String, String> getSelectYear() {
-		return selectYear;
-	}
-
-	public LinkedHashMap<String, String> getSelectMonth() {
-		return selectMonth;
-	}
-
-	public void setSelectMonth(LinkedHashMap<String, String> selectMonth) {
-		this.selectMonth = selectMonth;
-	}
-
-	public LinkedHashMap<String, String> getSelectWorkKind() {
-		return selectWorkKind;
-	}
-
-	public void setSelectWorkKind(LinkedHashMap<String, String> selectWorkKind) {
-		this.selectWorkKind = selectWorkKind;
-	}
-
+	
+	/**
+	 * コンボ年
+	 * @return
+	 */
 	public String getYear() {
 		return year;
 	}
 
+	/**
+	 * コンボ年
+	 * @param year
+	 */
 	public void setYear(String year) {
 		this.year = year;
 	}
-
+	
+	/**
+	 * コンボ月
+	 * @return
+	 */
 	public String getMonth() {
 		return month;
 	}
 
+	/**
+	 * コンボ月
+	 * @param month
+	 */
 	public void setMonth(String month) {
 		this.month = month;
 	}
 
+	/**
+	 * ログイン名
+	 * @return
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * ログイン名
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * 勤務表データ
+	 * @return
+	 */
 	public RosterDto[] getData() {
 		if ( data != null) return data;
 
@@ -134,34 +135,112 @@ public final class RosterBean extends ActionForm {
 		 return data;
 	}
 
+	/**
+	 * 勤務表データ
+	 * @param data
+	 */
 	public void setData(RosterDto[] data) {
 		this.data = data;
 	}
 
+	/**
+	 * 勤務表データ
+	 * @param index
+	 * @return
+	 */
 	public RosterDto getData(int index) {
-		/* TODOインデックス範囲チェック未実装 */
 		if(data == null) return  new RosterDto();
 		return data[index];
 	}
 
+	/**
+	 * 勤務表データ
+	 * @param index
+	 * @param data
+	 */
 	public void setData(int index, RosterDto[] data) {
-		/* TODOインデックス範囲チェック未実装  */
 		if(data == null) return;
 		this.data[index] = data[index];
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	/**
+	 * コンボ年（リスト内容）
+	 * @return
+	 */
+	public LinkedHashMap<String, String> getSelectYear() {
+		return selectYear;
 	}
 
-	public String getName() {
-		return name;
+	/**
+	 * コンボ年（リスト内容）
+	 * @param selectYear
+	 */
+	public void setSelectYear(LinkedHashMap<String, String> selectYear) {
+		this.selectYear = selectYear;
 	}
 
+	/**
+	 * コンボ月（リスト内容）
+	 * @return
+	 */
+	public LinkedHashMap<String, String> getSelectMonth() {
+		return selectMonth;
+	}
+
+	/**
+	 * コンボ月（リスト内容）
+	 * @param selectMonth
+	 */
+	public void setSelectMonth(LinkedHashMap<String, String> selectMonth) {
+		this.selectMonth = selectMonth;
+	}
+
+	/**
+	 * コンボ勤務種別（リスト内容）
+	 * @return
+	 */
+	public LinkedHashMap<String, String> getSelectWorkKind() {
+		return selectWorkKind;
+	}
+
+	/**
+	 * コンボ勤務種別（リスト内容）
+	 * @param selectWorkKind
+	 */
+	public void setSelectWorkKind(LinkedHashMap<String, String> selectWorkKind) {
+		this.selectWorkKind = selectWorkKind;
+	}
+
+	/**
+	 * アクションモード
+	 * 移動、保存、Excel出力を判断する
+	 * @return
+	 */
+	public String getActionMode() {
+		return actionMode;
+	}
+
+	/**
+	 * アクションモード
+	 * 移動、保存、Excel出力を判断する
+	 * @param actionMode
+	 */
+	public void setActionMode(String actionMode) {
+		this.actionMode = actionMode;
+	}
+
+	/**
+	 * キャッシュ対策用の時間
+	 * @return
+	 */
 	public String getTimeString() {
 		return timeString;
 	}
 
+	/**
+	 * キャッシュ対策用の時間
+	 * @param timeString
+	 */
 	public void setTimeString(String timeString) {
 		this.timeString = timeString;
 	}
